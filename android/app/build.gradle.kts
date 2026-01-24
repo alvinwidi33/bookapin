0387a1e4280304c72ece5e5fe8ca303a4bebccb2
Alvin Widi Nugroho <alvin.widi@ui.ac.id>
2026-01-24 12:18:18 +0700

    fixing

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -40,26 +40,33 @@ android {
 
     signingConfigs {
         create("release") {
-            storeFile = file(keystoreProperties["storeFile"] as String)
-            storePassword = keystoreProperties["storePassword"] as String
-            keyAlias = keystoreProperties["keyAlias"] as String
-            keyPassword = keystoreProperties["keyPassword"] as String
+            val storeFilePath = keystoreProperties["storeFile"] as String?
+            if (storeFilePath != null && file(storeFilePath).exists()) {
+                storeFile = file(storeFilePath)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+            }
         }
     }
 
     buildTypes {
-        getByName("release") {
+    getByName("release") {
+        val storeFilePath = keystoreProperties["storeFile"] as String?
+        if (storeFilePath != null && file(storeFilePath).exists()) {
             signingConfig = signingConfigs.getByName("release")
-
-            isMinifyEnabled = true
-            isShrinkResources = true
-
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
         }
+
+        isMinifyEnabled = true
+        isShrinkResources = true
+
+        proguardFiles(
+            getDefaultProguardFile("proguard-android-optimize.txt"),
+            "proguard-rules.pro"
+        )
     }
+}
+    
 }
 
 flutter {
